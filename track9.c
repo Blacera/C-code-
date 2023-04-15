b32f10600fd7ed4ff94a3f4146c562002e0a0ff2
Blacera <130491266+Blacera@users.noreply.github.com>
2023-04-16 01:34:47 +0700

    Add files via upload

---
+++ track9.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int n;
+    scanf ("%d",&n) ;
+    int lim = (n - 1) / 2; // giá trị của lim chính là giá trị ta cần tìm 
+    printf("%d",lim);
+    return 0;
+}
